3aeecf0777aa1ceac0c2ff4735d3445b1d0e4370
Sebastian Sanabria <sebastian@freelanders.co>
2025-10-17 10:25:25 -0500

    Reorganizar estructura: upload/ como carpeta pública

---
+++ upload/admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://hopuniverse.com/admin/');
+define('HTTP_CATALOG', 'https://hopuniverse.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://hopuniverse.com/admin/');
+define('HTTPS_CATALOG', 'https://hopuniverse.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/hopuniverse.com/admin/');
+define('DIR_SYSTEM', '/var/www/hopuniverse.com/system/');
+define('DIR_IMAGE', '/var/www/hopuniverse.com/upload/image/');
+define('DIR_STORAGE', '/var/www/hopuniverse_storage/');
+define('DIR_CATALOG', '/var/www/hopuniverse.com/catalog/');
+define('DIR_LANGUAGE', DIR_SYSTEM . 'library/language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'hopuniverse_user');
+define('DB_PASSWORD', 'HopUniverse2025!');
+define('DB_DATABASE', 'hopuniverse_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+
