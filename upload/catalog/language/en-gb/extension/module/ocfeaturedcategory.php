a1e3c19c51270a501dce2e20a0b1eaf3906e38be
Sebastian Sanabria <sebastian@freelanders.co>
2025-10-17 11:05:26 -0500

    Install Theme

---
+++ upload/catalog/language/en-gb/extension/module/ocfeaturedcategory.php
@@ -0,0 +1,4 @@
+<?php
+// Heading
+$_['text_headingtitle'] = 'Featured Categories';
+$_['text_subtitle'] 	= 'Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem.s';
