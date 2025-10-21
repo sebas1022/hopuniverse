729888cb6033959358cff7ff45be4c5ef9a8c68f
Sebastian Sanabria <sebastian@freelanders.co>
2025-10-21 16:58:34 -0500

    Añadir archivo de idioma español para módulo newsletter subscribe - editable desde admin/design/translation

---
+++ upload/catalog/language/es/extension/module/newslettersubscribe.php
@@ -0,0 +1,25 @@
+<?php
+//Fields
+$_['entry_title'] 		 = 'Suscríbete a Nuestro Boletín';
+$_['entry_description']  = 'Suscríbete a nuestros boletines ahora y mantente actualizado con las nuevas colecciones, los últimos catálogos y ofertas exclusivas.';
+$_['entry_email'] 		 = 'Correo Electrónico';
+$_['entry_name'] 		 = 'Nombre';
+
+//Buttons
+$_['entry_button'] 		 = 'Suscribirse';
+$_['entry_unbutton'] 	 = 'Cancelar Suscripción';
+
+//text
+$_['text_close'] 	     = 'Cerrar';
+$_['text_subscribe'] 	 = 'Suscríbete Aquí';
+
+$_['mail_subject']   	 = 'Suscripción al Boletín';
+
+//Messages
+$_['error_invalid'] 	 = 'Error: Por favor ingresa una dirección de correo electrónico válida.';
+$_['subscribe']	    	 = 'Éxito: Suscripción realizada correctamente.';
+$_['unsubscribe'] 	     = 'Éxito: Suscripción cancelada correctamente.';
+$_['alreadyexist'] 	     = 'Error: El correo electrónico ya existe.';
+$_['notexist'] 	    	 = 'Error: El correo electrónico no existe.';
+$_['entry_show_again']   =  "No mostrar esta ventana emergente de nuevo";
+
