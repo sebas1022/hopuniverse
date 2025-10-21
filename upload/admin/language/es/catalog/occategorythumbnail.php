e371049a4b11f517c8a600b0c8968049043f16d7
Sebastian Sanabria <sebastian@freelanders.co>
2025-10-21 09:43:16 -0500

    Add Spanish language files for thumbnails and testimonials modules + Update .gitignore

---
+++ upload/admin/language/es/catalog/occategorythumbnail.php
@@ -0,0 +1,35 @@
+<?php
+// Heading
+$_['heading_title']          = 'Miniaturas de Categorías';
+
+// Text
+$_['text_success']           = 'Éxito: ¡Has modificado las miniaturas de categorías!';
+$_['text_thumbnail']         = 'Miniaturas de Categorías';
+$_['text_list']              = 'Lista de Miniaturas de Categorías';
+$_['text_form']              = 'Editar Miniatura de Categoría';
+$_['text_edit']              = 'Editar Miniatura de Categoría';
+$_['text_yes']               = 'Sí';
+$_['text_no']                = 'No';
+$_['text_no_results']        = 'No hay resultados';
+$_['text_home']              = 'Inicio';
+
+// Column
+$_['column_name']            = 'Nombre de Categoría';
+$_['column_sort_order']      = 'Orden';
+$_['column_action']          = 'Acción';
+$_['column_thumbnail']       = 'Imagen Miniatura';
+$_['column_homethumb']       = 'Imagen Miniatura Hover';
+$_['column_featured']        = '¿Es Destacada?';
+
+// Entry
+$_['entry_name']             = 'Nombre de Categoría';
+$_['entry_thumbnail_image']  = 'Imagen Miniatura';
+$_['entry_homethumb_image']  = 'Imagen Miniatura Hover';
+$_['entry_featured']  		 = '¿Es Destacada?';
+
+// Button
+$_['button_edit']            = 'Editar';
+
+// Error
+$_['error_permission']       = 'Advertencia: ¡No tienes permiso para modificar las miniaturas de categorías!';
+
---
+++ upload/admin/language/es/catalog/octestimonial.php
@@ -0,0 +1,37 @@
+<?php
+// Heading
+$_['heading_title']     = '<b><i>Testimonios</i></b>';
+$_['page_title']     = 'Testimonios';
+
+// Text  
+$_['text_success']      = 'Éxito: ¡Has modificado los testimonios!';
+$_['text_list']      = 'Lista de testimonios';
+$_['text_form']      = 'Formulario de testimonio';
+$_['text_edit']      = 'Editar';
+$_['text_testimonial'] = 'Testimonios';
+$_['text_enabled']      = 'Habilitado';
+$_['text_disabled']     = 'Deshabilitado';
+
+// Column
+$_['column_name']       = 'Nombre del Cliente';
+$_['column_sort_order'] = 'Orden';
+$_['column_status']     = 'Estado';
+$_['column_action']     = 'Acción';
+
+// Entry
+$_['entry_name']        = 'Nombre del Cliente:';
+$_['entry_testimonial_limit'] = "Elementos por Página por Defecto";
+$_['entry_testimonial_image'] = "Tamaño de Imagen del Testimonio";
+$_['entry_image']        = 'Archivo de Imagen:';
+$_['entry_description'] = 'Contenido:';
+$_['entry_status']      = 'Estado:';
+$_['entry_sort_order']  = 'Orden:';
+
+// Error
+$_['error_warning']          = 'Advertencia: ¡Por favor revisa el formulario cuidadosamente!';
+$_['error_permission']       = 'Advertencia: ¡No tienes permiso para modificar los testimonios!';
+$_['error_name']       		 = '¡El nombre del cliente debe tener entre 3 y 255 caracteres!';
+$_['error_description'] 	 = '¡El contenido debe tener más de 3 caracteres!';
+$_['error_image_testimonial'] 	 = '¡Se requieren las dimensiones de la imagen del testimonio!';
+$_['error_testimonial_limit'] 	 = '¡Se requiere el límite de testimonios por página!';
+
