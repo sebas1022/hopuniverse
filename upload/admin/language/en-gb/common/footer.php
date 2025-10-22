7e7f05dbf7f850a92f1485e0359557030f081874
Sebastian Sanabria <sebastian@freelanders.co>
2025-10-22 17:00:57 -0500

    Update footer.php

--- upload/admin/language/en-gb/common/footer.php
+++ upload/admin/language/en-gb/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer']  = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer']  = '<a href="https://frubik.com/" target="_blank">Frubik</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] = 'Version %s';
