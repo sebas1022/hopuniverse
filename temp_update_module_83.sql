b8b390d0a0880126df8fa372df9543a3450569c5
Sebastian Sanabria <sebastian@freelanders.co>
2025-10-22 16:28:20 -0500

    Clean: Eliminar script SQL temporal ya ejecutado

--- temp_update_module_83.sql
+++
@@ -1,9 +0,0 @@
--- Actualizar módulo 83 con el nuevo HTML en español
-UPDATE oc_module 
-SET setting = JSON_SET(
-    setting,
-    '$.module_description."2".description',
-    '<div class="newsletter-text"><h5><b>OFERTAS ESPECIALES</b> PARA MIEMBROS DE HOP</h5>\n\t<h3>¡10% de descuento intergaláctico para nuevos reclutas!</h3>\n\tSuscríbete al boletín de HOP Universe y mantente al tanto de nuestras últimas novedades, historias exclusivas y colecciones de edición limitada.<br><p>Porque incluso en el espacio... ¡hay grandes ofertas!</p>\n</div>'
-)
-WHERE module_id = 83;
-
