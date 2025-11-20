c777bb82a2612958ba7863eb83a7a0f293cc4a61
Sebastian Sanabria <sebastian@freelanders.co>
2025-11-20 18:28:10 -0500

    Update voucher.php

--- upload/catalog/language/es/extension/total/voucher.php
+++ upload/catalog/language/es/extension/total/voucher.php
@@ -7,6 +7,13 @@ $_['text_extension']   = 'Extensiones';
 $_['text_success']     = 'Correcto: Ha modificado vales regalo.';
 $_['text_edit']        = 'Editar vales regalo';
 
+$_['entry_voucher'] = 'Ingresa tu c&oacute;digo de vale regalo aqu&iacute;';
+
+// Buttonq
+$_['button_apply'] = 'Aplicar';
+$_['button_voucher'] = 'Aplicar V&aacute;l Regalo';
+$_['text_loading'] = 'Cargando...';
+
 // Entry
 $_['entry_status']     = 'Estado:';
 $_['entry_sort_order'] = 'Orden de clasificaci&oacute;n:';
